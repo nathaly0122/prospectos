cbb8928b11bf1276508eeb0c41c6b09c007a8ab1
Grace <vecino6211@gmail.com>
2022-09-19 11:59:17 -0500

    proyecto inicial maven de spring boot

---
+++ prospectos/src/main/java/com/garces/prospectos/ProspectosApplication.java
@@ -0,0 +1,13 @@
+package com.garces.prospectos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProspectosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProspectosApplication.class, args);
+	}
+
+}
---
+++ prospectos/src/test/java/com/garces/prospectos/ProspectosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garces.prospectos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProspectosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
